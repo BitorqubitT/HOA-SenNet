0c142b678f1dcc22dcf24e90c30ccb3aa25103bc
thierry <thierryars2@hotmail.com>
2024-02-04 14:34:30 +0100

    training file hoa

---
+++ training.py
@@ -0,0 +1,312 @@
+#!mkdir -p /root/.cache/torch/hub/checkpoints/
+#!cp /kaggle/input/se-net-pretrained-imagenet-weights/* /root/.cache/torch/hub/checkpoints/
+import torch as tc 
+import torch.nn as nn  
+import numpy as np
+from tqdm import tqdm
+import os,sys,cv2
+from torch.cuda.amp import autocast
+import matplotlib.pyplot as plt
+import albumentations as A
+#!python -m pip install --no-index --find-links=/kaggle/input/pip-download-for-segmentation-models-pytorch segmentation-models-pytorch
+import segmentation_models_pytorch as smp
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.parallel import DataParallel
+from glob import glob
+
+
+
+p_augm = 0.05 #0.5
+#add rotate.  less p_augm
+
+class CFG:
+    # ============== pred target =============
+    target_size = 1
+
+    # ============== model CFG =============
+    model_name = 'Unet'
+    backbone = 'se_resnext50_32x4d'
+
+    # Change to 5 for 3d?
+    in_chans = 1   #5 # 65
+    # ============== training CFG =============
+    image_size = 1024 # 512 # 512
+    input_size = 1024 # 512 #=512
+
+    train_batch_size = 1 #4 #16
+    valid_batch_size = train_batch_size * 2
+
+    epochs = 25 #27 #30 #25
+    lr = 8e-5
+    chopping_percentile=1e-3
+    # ============== fold =============
+    valid_id = 1
+
+
+    # ============== augmentation =============
+    train_aug_list = [
+        A.Rotate(limit=270, p= 0.5),
+        A.RandomScale(scale_limit=(0.8,1.25),interpolation=cv2.INTER_CUBIC,p=p_augm),
+        A.RandomCrop(input_size, input_size,p=1),
+        A.RandomGamma(p=p_augm*2/3),
+        A.RandomBrightnessContrast(p=p_augm,),
+        A.GaussianBlur(p=p_augm),
+        A.MotionBlur(p=p_augm),
+        A.GridDistortion(num_steps=5, distort_limit=0.3, p=p_augm),
+        ToTensorV2(transpose_mask=True),
+    ]
+    train_aug = A.Compose(train_aug_list)
+    valid_aug_list = [
+        ToTensorV2(transpose_mask=True),
+    ]
+    valid_aug = A.Compose(valid_aug_list)
+
+
+
+# model
+    
+class CustomModel(nn.Module):
+    def __init__(self, CFG, weight=None):
+        super().__init__()
+        self.model = smp.FPN(  #FPN Unet
+            encoder_name=CFG.backbone, 
+            encoder_weights=weight,
+            in_channels=CFG.in_chans,
+            classes=CFG.target_size,
+            activation=None,
+        )
+
+    def forward(self, image):
+        output = self.model(image)
+        # output = output.squeeze(-1)
+        return output[:,0]#.sigmoid()
+
+def build_model(weight="imagenet"):
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    print('model_name', CFG.model_name)
+    print('backbone', CFG.backbone)
+
+    model = CustomModel(CFG, weight)
+
+    return model.cuda()
+
+
+# change size
+# put in helpers
+
+
+def to_1024(img , image_size = 1024):
+    if image_size > img.shape[1]:
+       img = np.rot90(img)
+       start1 = (CFG.image_size - img.shape[0])//2 
+       top =     img[0                    : start1,   0: img.shape[1] ]
+       bottom  = img[img.shape[0] -start1 : img.shape[0],   0 : img.shape[1] ]
+       img_result = np.concatenate((top,img,bottom ),axis=0)
+       img_result = np.rot90(img_result)
+       img_result = np.rot90(img_result)
+       img_result = np.rot90(img_result)
+    else :
+       img_result = img
+    return img_result
+
+def to_1024_no_rot(img, image_size = 1024):
+    if image_size > img.shape[0]:  
+       start1 = ( image_size - img.shape[0])//2
+       top =     img[0                    : start1,   0: img.shape[1] ]
+       bottom  = img[img.shape[0] -start1 : img.shape[0],   0 : img.shape[1] ]
+       img_result = np.concatenate((top,img,bottom ),axis=0)
+    else: 
+       img_result = img
+    return img_result
+
+#  add border
+def to_1024_1024(img  , image_size = 1024 ):
+     img_result = to_1024(img, image_size )
+     return img_result
+    
+#  drop border
+def to_original ( im_after, img, image_size = 1024 ):
+    top_ = 0
+    left_ = 0
+    if (im_after.shape[0] > img.shape[0]):
+             top_  = ( image_size - img.shape[0])//2 
+    if    (im_after.shape[1] > img.shape[1]) :
+             left_  = ( image_size - img.shape[1])//2  
+    if (top_>0)or (left_>0) :
+             img_result = im_after[top_                    : img.shape[0] + top_,   left_: img.shape[1] + left_ ]
+             #print(im_after.shape,'-->',img_result.shape)
+    else:
+             img_result = im_after
+    return img_result  
+
+
+# Functionssssssssss
+
+
+
+def min_max_normalization(x:tc.Tensor)->tc.Tensor:
+    """input.shape=(batch,f1,...)"""
+    shape=x.shape
+    if x.ndim>2:
+        x=x.reshape(x.shape[0],-1)
+    
+    min_=x.min(dim=-1,keepdim=True)[0]
+    max_=x.max(dim=-1,keepdim=True)[0]
+    if min_.mean()==0 and max_.mean()==1:
+        return x.reshape(shape)
+    
+    x=(x-min_)/(max_-min_+1e-9)
+    return x.reshape(shape)
+
+def norm_with_clip(x:tc.Tensor,smooth=1e-5):
+    dim=list(range(1,x.ndim))
+    mean=x.mean(dim=dim,keepdim=True)
+    std=x.std(dim=dim,keepdim=True)
+    x=(x-mean)/(std+smooth)
+    x[x>5]=(x[x>5]-5)*1e-3 +5
+    x[x<-3]=(x[x<-3]+3)*1e-3-3
+    return x
+
+def add_noise(x:tc.Tensor,max_randn_rate=0.1,randn_rate=None,x_already_normed=False):
+    """input.shape=(batch,f1,f2,...) output's var will be normalizate  """
+    ndim=x.ndim-1
+    if x_already_normed:
+        x_std=tc.ones([x.shape[0]]+[1]*ndim,device=x.device,dtype=x.dtype)
+        x_mean=tc.zeros([x.shape[0]]+[1]*ndim,device=x.device,dtype=x.dtype)
+    else: 
+        dim=list(range(1,x.ndim))
+        x_std=x.std(dim=dim,keepdim=True)
+        x_mean=x.mean(dim=dim,keepdim=True)
+    if randn_rate is None:
+        randn_rate=max_randn_rate*np.random.rand()*tc.rand(x_mean.shape,device=x.device,dtype=x.dtype)
+    cache=(x_std**2+(x_std*randn_rate)**2)**0.5
+    return (x-x_mean+tc.randn(size=x.shape,device=x.device,dtype=x.dtype)*randn_rate*x_std)/(cache+1e-7)
+ 
+class Data_loader(Dataset):
+     
+    def __init__(self,paths,is_label):
+        self.paths=paths
+        self.paths.sort()
+        self.is_label=is_label
+    
+    def __len__(self):
+        return len(self.paths)
+    
+    def __getitem__(self,index):
+         
+        img = cv2.imread(self.paths[index],cv2.IMREAD_GRAYSCALE)
+        
+        img = to_1024_1024(img , image_size = CFG.image_size ) #  to_original( im_after, img_save, image_size = 1024)
+
+        img = tc.from_numpy(img.copy())
+        if self.is_label:
+            img=(img!=0).to(tc.uint8)*255
+        else:
+            img=img.to(tc.uint8)
+        return img
+
+def load_data(paths,is_label=False):
+    data_loader=Data_loader(paths,is_label)
+    data_loader=DataLoader(data_loader, batch_size=16, num_workers=2)  
+    data=[]
+    for x in tqdm(data_loader):
+        data.append(x)
+    x=tc.cat(data,dim=0)
+    del data
+    if not is_label:
+        ########################################################################
+        TH=x.reshape(-1).numpy()
+        index = -int(len(TH) * CFG.chopping_percentile)
+        TH:int = np.partition(TH, index)[index]
+        x[x>TH]=int(TH)
+        ########################################################################
+        TH=x.reshape(-1).numpy()
+        index = -int(len(TH) * CFG.chopping_percentile)
+        TH:int = np.partition(TH, -index)[-index]
+        x[x<TH]=int(TH)
+        ########################################################################
+        x=(min_max_normalization(x.to(tc.float16)[None])[0]*255).to(tc.uint8)
+    return x
+
+
+#https://www.kaggle.com/code/kashiwaba/sennet-hoa-train-unet-simple-baseline
+def dice_coef(y_pred:tc.Tensor,y_true:tc.Tensor, thr=0.5, dim=(-1,-2), epsilon=0.001):
+    y_pred=y_pred.sigmoid()
+    y_true = y_true.to(tc.float32)
+    y_pred = (y_pred>thr).to(tc.float32)
+    inter = (y_true*y_pred).sum(dim=dim)
+    den = y_true.sum(dim=dim) + y_pred.sum(dim=dim)
+    dice = ((2*inter+epsilon)/(den+epsilon)).mean()
+    return dice
+
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = inputs.sigmoid()   
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        # how do this give you an intersection?
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+        return 1 - dice
+
+class Kaggld_Dataset(Dataset):
+    def __init__(self,x:list,y:list,arg=False):
+        super(Dataset,self).__init__()
+        self.x=x#list[(C,H,W),...]
+        self.y=y#list[(C,H,W),...]
+        self.image_size=CFG.image_size
+        self.in_chans=CFG.in_chans
+        self.arg=arg
+        if arg:
+            self.transform=CFG.train_aug
+        else: 
+            self.transform=CFG.valid_aug
+
+    def __len__(self) -> int:
+        return sum([y.shape[0]-self.in_chans for y in self.y])
+    
+    def __getitem__(self,index):
+        i=0
+        for x in self.x:
+            if index>x.shape[0]-self.in_chans:
+                index-=x.shape[0]-self.in_chans
+                i+=1
+            else:
+                break
+        x=self.x[i]
+        y=self.y[i]
+        
+        print (f'x.shape[1] ={x.shape[1]}    x.shape[2]={x.shape[2]}')
+        
+        x_index= (x.shape[1]-self.image_size)//2 #np.random.randint(0,x.shape[1]-self.image_size)
+        y_index= (x.shape[2]-self.image_size)//2 # np.random.randint(0,x.shape[2]-self.image_size)
+        # i i+5 
+        x=x[index:index+self.in_chans   ,   x_index:x_index+self.image_size,   y_index:y_index+self.image_size]
+        # i+2
+        y=y[index+self.in_chans//2   ,      x_index:x_index+self.image_size,   y_index:y_index+self.image_size]
+
+        data = self.transform(image=x.numpy().transpose(1,2,0), mask=y.numpy())
+        x = data['image']
+        y = data['mask']>=127
+        if self.arg:
+            i=np.random.randint(4)
+            x=x.rot90(i,dims=(1,2))
+            y=y.rot90(i,dims=(0,1))
+            for i in range(3):
+                if np.random.randint(2):
+                    x=x.flip(dims=(i,))
+                    if i>=1:
+                        y=y.flip(dims=(i-1,))
+        return x,y#(uint8,uint8)
