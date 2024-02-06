42d94c8790df399cb51e0688a678e109c6799395
thierry <thierryars2@hotmail.com>
2024-02-06 22:16:13 +0100

    Added a param button

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from time import time
 
 import numpy as np
-from nilearn import image
 from skimage import draw, filters, exposure, measure
 from scipy import ndimage
 import os
@@ -15,19 +14,32 @@ from dash import html
 from dash import dcc
 from dash_slicer import VolumeSlicer
 import tifffile
-from scipy.ndimage import median_filter
 import torch
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, update_title=None, external_stylesheets=external_stylesheets)
 server = app.server
 
 
-t1 = time()
+# TODO:
+# check how the input from a button is given
+# Chek how they define a model then put it in a button and update it
+# Maybe just wrap my code in a function and put that underneath a callback
+
+# check populate_bigram_scatter for this
+# line 724 -> perplexity>??????
+
+# Add two buttons for the histogram
+
+# Cleanup code, (format, remove useless stuff)
+
+# Add other graphs (A way to view and compare mask, training slices?)
+# Other graph shows score?
+
+# remove these timers 
 
 # ------------- I/O and data massaging ---------------------------------------------------
 
 # Is there  a way to compute this on gpu?
-
 labels_folder_path = 'D:/data/train/kidney_3_sparse/labels'  # Adjust the path accordingly
 label_images = []
 for file in sorted(os.listdir(labels_folder_path))[200:800]:
@@ -38,37 +50,13 @@ for file in sorted(os.listdir(labels_folder_path))[200:800]:
 
 # Combine the first 100 images into one 3D tensor
 all_images = torch.stack(label_images, dim=0)
-print(all_images.shape)
 
 # Extract coordinates of non-zero values (assuming binary segmentation)
 non_zero_coords = all_images.nonzero()
-
-print(type(non_zero_coords))
-print(non_zero_coords.shape)
-print(non_zero_coords[0])
-print(non_zero_coords[1])
-print(non_zero_coords[200])
 med_img = non_zero_coords
-
-#mat = img.affine
-#img = img.get_fdata()
-#img = np.copy(np.moveaxis(img, -1, 0))[:, ::-1]
-
-#spacing = abs(mat[2, 2]), abs(mat[1, 1]), abs(mat[0, 0])
-
-
-
-# Create smoothed image and histogram
-#med_img = filters.median(img, size=np.ones((1, 3, 3), dtype=bool))
-#med_img = median_filter(img, size=np.ones((1, 3, 3), dtype=bool))
-#med_img = median_filter(img, size=(1, 3, 3))
-#hi = exposure.histogram(med_img)
-
 med_img = all_images.numpy()
 
 # Create mesh
-print(type(med_img))
-print(med_img.shape)
 # Step_size affects the level of detail and 200 is threshold. When volume over this threshold we graph it.
 # Marching cube reduces the data complexity. 
 # Without MC you dont have any surface roconstruction or isosurface extraction
@@ -77,8 +65,6 @@ x, y, z = verts.T
 i, j, k = faces.T
 fig_mesh = go.Figure()
 fig_mesh.add_trace(go.Mesh3d(x=z, y=y, z=x, opacity=0.2, i=k, j=j, k=i))
-
-
 
 def path_to_coords(path):
     """From SVG path to numpy array of coordinates, each row being a (row, col) point"""
@@ -94,9 +80,6 @@ def largest_connected_component(mask):
     return labels == (np.argmax(sizes) + 1)
 
 
-t2 = time()
-print("initial calculations", t2 - t1)
-
 # ------------- Define App Layout ---------------------------------------------------
 
 mesh_card = dbc.Card(
@@ -106,11 +89,8 @@ mesh_card = dbc.Card(
     ]
 )
 
-# Define Modal
-with open("assets/modal.md", "r") as f:
-    howto_md = f.read()
-
 # Buttons
+# Can I remove these buttons?
 button_gh = dbc.Button(
     "Learn more",
     id="howto-open",
@@ -127,6 +107,17 @@ button_howto = dbc.Button(
     href="https://github.com/plotly/dash-sample-apps/tree/master/apps/dash-covid-xray",
     id="gh-link",
     style={"text-transform": "none"},
+)
+
+
+# New button which holds params that can alter the processing of the image
+# Check other app, might need to set col + html title 
+button_stepsize = dcc.Dropdown(
+    id = "set_stepsize",
+    options = [{"label": str(i), "value": i}
+               for i in range(3, 7)
+              ],
+    value=3,
 )
 
 nav_bar = dbc.Navbar(
@@ -168,6 +159,7 @@ app.layout = html.Div(
         dbc.Container(
             [
                 dbc.Row([dbc.Col(mesh_card),]),
+                dbc.Row([dbc.Col(button_stepsize),]),
             ],
             fluid=True,
         ),
@@ -176,8 +168,6 @@ app.layout = html.Div(
     ],
 )
 
-t3 = time()
-print("layout definition", t3 - t2)
 
 
 # ------------- Define App Interactivity ---------------------------------------------------
@@ -218,70 +208,10 @@ def update_histo(annotations):
 
 
 @app.callback(
-    [
-        Output("occlusion-surface", "data"),
-    ],
-    [Input("graph-histogram", "selectedData"), Input("annotations", "data")],
-)
-def update_segmentation_slices(selected, annotations):
-    ctx = dash.callback_context
-    # When shape annotations are changed, reset segmentation visualization
-    if (
-        ctx.triggered[0]["prop_id"] == "annotations.data"
-        or annotations is None
-        or annotations.get("x") is None
-        or annotations.get("z") is None
-    ):
-        mask = np.zeros_like(med_img)
-        overlay1 = slicer1.create_overlay_data(mask)
-        overlay2 = slicer2.create_overlay_data(mask)
-        return go.Mesh3d(), overlay1, overlay2
-    elif selected is not None and "range" in selected:
-        if len(selected["points"]) == 0:
-            return dash.no_update
-        v_min, v_max = selected["range"]["x"]
-        t_start = time()
-        # Horizontal mask
-        path = path_to_coords(annotations["z"]["path"])
-        rr, cc = draw.polygon(path[:, 1] / spacing[1], path[:, 0] / spacing[2])
-        mask = np.zeros(img.shape[1:])
-        mask[rr, cc] = 1
-        mask = ndimage.binary_fill_holes(mask)
-        # top and bottom, the top is a lower number than the bottom because y values
-        # increase moving down the figure
-        top, bottom = sorted(
-            [int(annotations["x"][c] / spacing[0]) for c in ["y0", "y1"]]
-        )
-        img_mask = np.logical_and(med_img > v_min, med_img <= v_max)
-        img_mask[:top] = False
-        img_mask[bottom:] = False
-        img_mask[top:bottom, np.logical_not(mask)] = False
-        img_mask = largest_connected_component(img_mask)
-        # img_mask_color = mask_to_color(img_mask)
-        t_end = time()
-        print("build the mask", t_end - t_start)
-        t_start = time()
-        # Update 3d viz
-        verts, faces, _, _ = measure.marching_cubes(
-            filters.median(img_mask, selem=np.ones((1, 7, 7))), 0.5, step_size=3
-        )
-        t_end = time()
-        print("marching cubes", t_end - t_start)
-        x, y, z = verts.T
-        i, j, k = faces.T
-        trace = go.Mesh3d(x=z, y=y, z=x, color="red", opacity=0.8, i=k, j=j, k=i)
-        overlay1 = slicer1.create_overlay_data(img_mask)
-        overlay2 = slicer2.create_overlay_data(img_mask)
-        # todo: do we need an output to trigger an update?
-        return trace, overlay1, overlay2
-    else:
-        return (dash.no_update,) * 3
-
-
-@app.callback(
     Output("annotations", "data"),
     [State("annotations", "data")],
 )
+
 def update_annotations(relayout1, relayout2, annotations):
     if relayout1 is not None and "shapes" in relayout1:
         if len(relayout1["shapes"]) >= 1:
@@ -305,7 +235,6 @@ def update_annotations(relayout1, relayout2, annotations):
         annotations["x"]["y1"] = relayout2["shapes[2].y1"]
     return annotations
 
-
 app.clientside_callback(
     """
 function(surf, fig){
@@ -318,7 +247,6 @@ function(surf, fig){
     inputs=[Input("occlusion-surface", "data"),],
     state=[State("graph-helper", "figure"),],
 )
-
 
 @app.callback(
     Output("modal", "is_open"),
